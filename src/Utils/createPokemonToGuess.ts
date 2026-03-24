795d25010aee4154a2f88f02b9a3686b5ddcf381
Ingrid <ingridstanic@gmail.com>
2026-03-24 13:32:43 +0100

    added more styles to module

--- src/Utils/createPokemonToGuess.ts
+++ src/Utils/createPokemonToGuess.ts
@@ -11,6 +11,11 @@ export const createPokemonToGuess = (pokemon: Pokemon) => {
     pokemonContainer.innerHTML = "";
   }
 
+  const module = document.createElement("div");
+  const pokeballContainer = document.createElement("ul");
+  const pokeball1 = document.createElement("li");
+  const pokeball2 = document.createElement("li");
+  const pokeball3 = document.createElement("li");
   const imgContainer = document.createElement("div");
   const pokemonImg = document.createElement("img");
   const guessContainer = document.createElement("div");
@@ -18,6 +23,16 @@ export const createPokemonToGuess = (pokemon: Pokemon) => {
   const guessInput = document.createElement("input");
   const guessBtn = document.createElement("button");
 
+  module.id = "moduleContainer";
+  module.className = "module-container";
+  pokeballContainer.id = "pokeballContainer";
+  pokeballContainer.className = "pokeball-container";
+  pokeball1.innerHTML =
+    '<img src="../src/assets/pokeball.png" alt="Pokéball" >';
+  pokeball2.innerHTML =
+    '<img src="../src/assets/pokeball.png" alt="Pokéball" >';
+  pokeball3.innerHTML =
+    '<img src="../src/assets/pokeball.png" alt="Pokéball" >';
   imgContainer.className = "img-container";
   pokemonImg.className = "pokemon-img";
   guessContainer.className = "guess-container";
@@ -49,14 +64,20 @@ export const createPokemonToGuess = (pokemon: Pokemon) => {
   pokemonImg.src = pokemon.sprites.front_default;
   pokemonImg.alt = "Pokemon to guess";
 
+  pokeballContainer.appendChild(pokeball1);
+  pokeballContainer.appendChild(pokeball2);
+  pokeballContainer.appendChild(pokeball3);
   imgContainer.appendChild(pokemonImg);
   guessForm.appendChild(guessInput);
   guessForm.appendChild(guessBtn);
 
   guessContainer.appendChild(guessForm);
 
-  pokemonContainer?.appendChild(imgContainer);
-  pokemonContainer?.appendChild(guessContainer);
+  module.appendChild(imgContainer);
+  module.appendChild(pokeballContainer);
+  module.appendChild(guessContainer);
+
+  pokemonContainer?.appendChild(module);
 };
 
 export const nextPokemon = () => {
