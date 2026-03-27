bc24e7376d9a18d18e7c6c39001dd8a4e9939962
Ingrid <ingridstanic@gmail.com>
2026-03-27 11:42:14 +0100

    change to img src

---
+++ src/Utils/headerUtil.ts
@@ -0,0 +1,32 @@
+export const createHeader = () => {
+  const headerContainer = document.getElementById("headerContainer");
+
+  if (!headerContainer) return;
+
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+
+  imgContainer.className = "img-container";
+  img.src = "../assets/PokemonWho.png";
+  img.alt = "Whos that pokemon";
+
+  imgContainer.appendChild(img);
+  headerContainer.appendChild(imgContainer);
+};
+
+// export const createNavbar = () => {
+//   const headerContainer = document.getElementById("headerContainer");
+
+//   if (!headerContainer) return;
+
+//   const nav = document.createElement("nav");
+//   const ul = document.createElement("ul");
+//   const li = document.createElement("li");
+
+//   nav.id = "navContainer";
+//   nav.className = "nav-container";
+
+//   li.innerHTML = "POKÉDEX";
+
+//   ul.appendChild(li);
+// };
